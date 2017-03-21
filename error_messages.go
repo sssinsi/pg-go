1475175d32b23157daf20e85f475e5be407bfce1
sssinsi <tet643773@gmail.com>
2017-03-22 00:03:55 +0900

    change to const

--- error_messages.go
+++ error_messages.go
@@ -5,11 +5,16 @@ import (
 	"strings"
 )
 
+const (
+	ErrorCode = "ErrCode"
+	ErrorInfo = "ErrInfo"
+)
+
 func ConvertToErrorResponses(vs url.Values) *ErrorResponses {
 	ers := &ErrorResponses{}
-	if vs.Get("ErrCode") != "" {
-		errCodeStrings := strings.Split(vs.Get("ErrCode"), "|")
-		errInfoStrings := strings.Split(vs.Get("ErrInfo"), "|")
+	if vs.Get(ErrorCode) != "" {
+		errCodeStrings := strings.Split(vs.Get(ErrorCode), "|")
+		errInfoStrings := strings.Split(vs.Get(ErrorInfo), "|")
 		for i := range errCodeStrings {
 			er := ErrorResponse{
 				Code: errCodeStrings[i],
